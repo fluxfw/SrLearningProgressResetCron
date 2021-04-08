5849cf213a22cf45ae2dc123ea3e1fc5bcba4d68
fluxfw <fluxfw@fluxlabs.ch>
2021-04-08 08:31:08 +0200

    Fix path to main plugin

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '3935d21eaac603d430e001e0d8969fcd' => '/UIComponent/UserInterfaceHook/SrLearningProgressReset/vendor/autoload.php',
+    '3935d21eaac603d430e001e0d8969fcd' => $baseDir . '/../../../UIComponent/UserInterfaceHook/SrLearningProgressReset/vendor/autoload.php',
 );
